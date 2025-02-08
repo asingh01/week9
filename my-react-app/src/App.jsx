5090413027ef0e67ffacf6e076927a9a601f7ded
abhijeet <abc.xyz.com>
2025-02-09 02:11:40 +0530

    regular comit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,23 +6,29 @@ const [tabdata, setTabdata] = useState({})
 const [loading, setLoading] = useState(false)
 
 useEffect(() => {
+  document.title = "Todo#" + currentTab
+},[])
+
+useEffect(() => {
     setLoading(true)
 //   console.log("Send backend request for: " + currentTab)
   fetch('https://jsonplaceholder.typicode.com/todos/'+currentTab)
       .then(async response => {
-        const json = await response.json()
-        setTabdata(json);setLoading(false)});
-    }, [currentTab])
+                                const json = await response.json()
+                                setTabdata(json);
+                                setLoading(false)
+                              })
+      }, [currentTab])
 
   return (
     <div>
-      <button onClick = {function(){setCurrentTab(1)}}style={{color: currentTab==1?"red":"black"}}>Todo#1</button>
-      <button onClick = {function(){setCurrentTab(2)}}style={{color: currentTab==2?"red":"black"}}>Todo#2</button>
-      <button onClick = {function(){setCurrentTab(3)}}style={{color: currentTab==3?"red":"black"}}>Todo#3</button>
-      <button onClick = {function(){setCurrentTab(4)}}style={{color: currentTab==4?"red":"black"}}>Todo#4</button>
+      <button onClick = {function(){setCurrentTab(1)}}style={{backgroundColor:'beige', color: currentTab==1?"red":"black"}}>Todo#1</button>
+      <button onClick = {function(){setCurrentTab(2)}}style={{backgroundColor:'yellow',color: currentTab==2?"red":"black"}}>Todo#2</button>
+      <button onClick = {function(){setCurrentTab(3)}}style={{backgroundColor:'lightcoral',color: currentTab==3?"red":"black"}}>Todo#3</button>
+      <button onClick = {function(){setCurrentTab(4)}}style={{backgroundColor:'skyblue',color: currentTab==4?"red":"black"}}>Todo#4</button>
 
       {/* {JSON.stringify(tabdata)} */}
-      {loading?"Loading...":tabdata.title}
+      <p>{loading?"Loading...":tabdata.title}</p>
     </div>
   )
 }
